a8949076db85faea4aec9c7eb622683c096d5251
gautam jain <gautamjain572@gmail.com>
2025-01-06 18:09:59 +0530

    caraousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { HomeComponent } from './home/home.component';
 import { LayoutModule } from './layout/layout.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CarouselModule,
   ],
   providers: [
     provideClientHydration(withEventReplay()),
